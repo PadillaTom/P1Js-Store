b291c90627b16b67cf715562878df86efdfe1049
Tomas Padilla <padillatomasagustin@gmail.com>
2020-08-31 10:58:25 +0200

    Search Filter

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,14 +1,20 @@
-// global imports
+// Global imports
 import '../toggleSidebar.js';
 import '../cart/toggleCart.js';
 import '../cart/setupCart.js';
 
-//  filter imports
+// Filter imports
 import setupSearch from '../filters/search.js';
 import setupCompanies from '../filters/companies.js';
 import setupPrice from '../filters/price.js';
 
-// specific imports
+// Specific imports
 import { store } from '../store.js';
 import display from '../displayProducts.js';
 import { getElement } from '../utils.js';
+
+// Display la Store: (Que, Donde)
+const loading = getElement('.page-loading');
+display(store, getElement('.products-container'));
+loading.style.display = 'none';
+// Seleccionamos LOADING, DISPLAY la store, Removemos Loading
